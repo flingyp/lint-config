9408993d0898705a7a05bdca510f4b9a4c555789
YePeng <1213563369@qq.com>
2023-10-18 18:26:16 +0800

    feat: add basic rules and typescript rule

--- packages/eslint-config-basic/index.js
+++ packages/eslint-config-basic/index.js
@@ -1,7 +1,30 @@
 module.exports = {
   rules: {
+    'no-undef': 'off',
     'no-console': 'off',
-    'eol-last': ['error', 'always'],
-    'no-unused-expressions': ['error', { allowShortCircuit: true }],
+    'no-tabs': 'off',
+    'no-multi-assign': 'off',
+    'no-param-reassign': 'off',
+    'no-plusplus': 'off',
+    'no-shadow': 'warn',
+    'no-underscore-dangle': 'warn',
+    'no-unused-vars': 'warn',
+    'no-unused-expressions': 'off',
+    'no-useless-escape': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'array-callback-return': 'off',
+    'consistent-return': 'off',
+    'guard-for-in': 'off',
+    'func-names': 'off',
+    eqeqeq: 'warn',
+    'max-len': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: true, optionalDependencies: false, peerDependencies: false, bundledDependencies: false,
+    }],
+    'no-use-before-define': ['error', {
+      functions: false, classes: false, variables: true, allowNamedExports: true,
+    }],
   },
 };
--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -9,6 +9,7 @@ module.exports = {
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/ban-types': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
       },
     },
   ],
