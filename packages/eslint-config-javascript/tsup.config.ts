597c69908ca7b13263cccede3aac9593a7cf9562
flingyp <flingyp@163.com>
2025-07-17 16:16:03 +0800

    refactor(packages): 删除 eslint-config-javascript 子包

--- packages/eslint-config-javascript/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['./index.ts'],
-  format: ['esm'], // 只使用 ESM 格式
-  dts: true,
-  clean: true,
-  target: 'es2020',
-});
